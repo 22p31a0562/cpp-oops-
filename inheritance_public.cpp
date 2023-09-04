74e9acd6534c6f9bbe7faafed144cbeaf2f8423c
Peluri Ramya <119730513+22p31a0562@users.noreply.github.com>
2023-09-04 14:29:00 +0530

    Add files via upload

---
+++ inheritance_public.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+class Person{
+	public:
+		string name;
+		int age;
+		void Getname(string n)
+		{
+			name=n;
+		}
+		void Getage(int a)
+		{
+			age=a;
+		}
+	};
+class Student : public Person{
+	public:
+		int id;
+		string college;
+		void Getid(int i)
+		{
+			id=i;
+		}
+		void Getcollege(string clg)
+		{
+			college=clg;
+		}
+		void display()
+		{
+			cout<<"myself"<<" "<<name<<" "<<" with an age"<<" "<<age<<" "<<"studying in"<<" "<<college;
+		}
+};
+
+int main()
+{
+	Student p;
+	p.Getname("ramya");
+	p.Getage(18);
+	p.Getcollege("aditya");
+	p.display();
+}
